04a8819d2ef2ca18ac59aa9b2fb660668b3c2191
Tornike <itorn9777@gmail.com>
2023-02-21 18:23:36 +0400

    create redirects for netlify

--- data.js
+++ data.js
@@ -14,12 +14,22 @@ const planetsData = [
     name: 'mercury',
     texture: mercTexture,
     next: 'venus',
+    text: 'Mercury is the smallest planet in the Solar System and the closest to the Sun. It is named after the Roman, messenger of the gods, Mercurius. Mercury appears to have a solid silicate crust and mantle overlying a solid, iron sulfide outer core layer, a deeper liquid core layer, and a solid inner core.',
+    rotation: '59 days',
+    orbital_period: '88 days',
+    avg_temp: '167',
+    diameter: '4880'
   },
   {
     name: 'venus',
     texture: venusAtmosphereTexture,
     prev: 'mercury',
     next: 'earth',
+    text: 'Venus is the second planet from the Sun. It is sometimes called Earth\'s "sister" or "twin" planet as it is almost as large and has a similar composition. As an interior planet to Earth, Venus (like Mercury) appears in Earth\'s sky never far from the Sun, either as morning star or evening star. Aside from the Sun and Moon, Venus is the brightest natural object in Earth\'s sky, capable of casting visible shadows on Earth in dark conditions and being visible to the naked eye in broad daylight.',
+    rotation: '243 days',
+    orbital_period: '225 days',
+    avg_temp: '464',
+    diameter: '12,104'
   },
   {
     name: 'earth',
@@ -27,35 +37,65 @@ const planetsData = [
     clouds: earthCloudsTexture,
     prev: 'venus',
     next: 'mars',
+    text: 'Earth is the third planet from the Sun and the only place known in the universe where life has originated and found habitability. While large volumes of water can be found throughout the Solar System, only Earth sustains liquid surface water. Approximately 70.8% of Earth\'s surface is made up of the ocean, dwarfing Earth\'s polar ice, lakes, and rivers. The remaining 29.2% of Earth\'s surface is land, consisting of continents and islands.',
+    rotation: '24 hours',
+    orbital_period: '365 days',
+    avg_temp: '15',
+    diameter: '12,742'
   },
   {
     name: 'mars',
     texture: marsTexture,
     prev: 'earth',
-    next: 'jupiter'
+    next: 'jupiter',
+    text: 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, larger only than Mercury. In the English language, Mars is named for the Roman god of war. Mars is a terrestrial planet with a thin atmosphere and has a crust primarily composed of elements similar to Earth\'s crust, as well as a core made of iron and nickel. Mars has surface features such as impact craters, valleys, dunes, and polar ice caps. Mars has two small, irregularly shaped moons, Phobos and Deimos.',
+    rotation: '24.6 hours',
+    orbital_period: '687 days',
+    avg_temp: '-65',
+    diameter: '6,779'
   },
   {
     name: 'jupiter',
     texture: jupiterTexture,
     prev: 'mars',
     next: 'saturn',
+    text: 'Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass more than two and a half times that of all the other planets in the Solar System combined, and slightly less than one one-thousandth the mass of the Sun. Jupiter is the third brightest natural object in the Earth\'s night sky after the Moon and Venus, and it has been observed since prehistoric times. It was named after Jupiter, the chief deity of ancient Roman religion.',
+    rotation: '10 hours',
+    orbital_period: '12 years',
+    avg_temp: '-110',
+    diameter: '143,000'
   },
   {
     name: 'saturn',
     texture: saturnTexture,
     prev: 'jupiter',
     next: 'uranus',
+    text: 'Saturn is the sixth planet from the Sun and the second-largest in the Solar System, after Jupiter. It is a gas giant with an average radius of about nine and a half times that of Earth. It has only one-eighth the average density of Earth, but is over 95 times more massive. Saturn has a pale yellow hue due to ammonia crystals in its upper atmosphere. An electrical current within the metallic hydrogen layer is thought to give rise to Saturn\'s planetary magnetic field, which is weaker than Earth\'s, but which has a magnetic moment 580 times that of Earth due to Saturn\'s larger size.',
+    rotation: '10.7 hours',
+    orbital_period: '29 years',
+    avg_temp: '-140',
+    diameter: '116,460'
   },
   {
     name: 'uranus',
     texture: uranusTexture,
     prev: 'saturn',
     next: 'neptune',
+    text: 'Uranus has the third-largest planetary radius and fourth-largest planetary mass in the Solar System. The planet is similar in composition to Neptune, and both have bulk chemical compositions which differ from those of the other two giant planets, Jupiter and Saturn (the gas giants). For this reason, scientists often distinguish Uranus and Neptune as "ice giants". As with gas giants, ice giants lack a well-defined solid surface.',
+    rotation: '17 hours',
+    orbital_period: '84 years',
+    avg_temp: '-195',
+    diameter: '50,724'
   },
   {
     name: 'neptune',
     texture: neptuneTexture,
-    prev: 'uranus'
+    prev: 'uranus',
+    text: 'Neptune is the eighth planet from the Sun and the farthest known planet in the Solar System. It is the fourth-largest planet in the Solar System by diameter, the third-most-massive planet, and the densest giant planet. It is 17 times the mass of Earth, and slightly more massive than its near-twin Uranus. Neptune is denser and physically smaller than Uranus because its greater mass causes more gravitational compression of its atmosphere. Being composed primarily of gases and liquids, it has no well-defined solid surface.',
+    rotation: '19 hours',
+    orbital_period: '165 years',
+    avg_temp: '-200',
+    diameter: '49,244'
   },
 ]
 
--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -17,10 +17,30 @@ const Details = ({ setPlanet }) => {
 
 
   if (data) {
+    const { name, text, rotation, orbital_period, avg_temp, diameter} = data[0];
     setPlanet(data[0].name)
     return (
       <div className="Details">
-        <h2 className="Details-heading">{data[0].name}</h2>
+        <h2 className="Details-heading">{name}</h2>
+        <p>{text}</p>
+        <div className="Details-details">
+          <div className="detail">
+            <div>Rotation</div>
+            <div>{rotation}</div>
+          </div>
+          <div className="detail">
+            <div>Orbital period</div>
+            <div>{orbital_period}</div>
+          </div>
+          <div className="detail">
+            <div>Avarage temp</div>
+            <div>{avg_temp} C&deg;</div>
+          </div>
+          <div className="detail">
+            <div>Diameter</div>
+            <div>{diameter} km</div>
+          </div>
+        </div>
         <div className="pagination">
           {data[0].prev && (
             <Link to={`/${data[0].prev}`}>
